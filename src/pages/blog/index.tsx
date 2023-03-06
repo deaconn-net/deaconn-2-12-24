519b7da3b2ee05dafbbd92aa68a7b1de5145f584
Christian Deacon <gamemann@gflclan.com>
2023-03-05 20:49:19 -0600

    Improve articles and blog; Start index progress.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import { GetServerSidePropsContext, NextPage } from "next";
 import { api } from "~/utils/api";
 import { Deaconn } from '../../components/main';
 
@@ -6,21 +6,45 @@ import InfiniteScroll from 'react-infinite-scroller';
 import { Article } from "@prisma/client";
 import { Loader } from "~/components/utils/loader";
 import { useState } from "react";
+import Link from "next/link";
 
-const ArticleRow: React.FC<{ article: Article }> = ({ article }) => {
+export const ArticleRow: React.FC<{ article: Article, cdn: string, small?: boolean }> = ({ article , cdn, small=false }) => {
+  const viewUrl = "/blog/view/" + article.url;
+  const banner = (article.banner) ? cdn + article.banner : "/images/blog/default.jpg";
+
   return (
-    <div className="p-12 bg-gray-500">
-      <h3 className="text-white text-xl font-bold">{article.title}</h3>
-      <p className="text-gray-100">{article.desc}</p>
+    <div className={"article-row " + ((small) ? "article-row-sm" : "article-row-lg")}>
+      <div className="w-full h-72">
+        <img src={banner} className="w-full h-full max-h-full" />
+      </div>
+      <div className="">
+        <h3 className="text-white text-2xl font-bold text-center">{article.title}</h3>
+      </div>
+      <div className="pb-6 grow">
+        <p className="text-gray-100 text-sm">{article.desc}</p>
+      </div>
+      <div className="pb-6 flex justify-between text-white text-sm">
+        <div className="flex flex-wrap items-center">
+          <span><svg className="w-6 h-6 fill-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9ZM11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12Z" /><path fillRule="evenodd" clipRule="evenodd" d="M21.83 11.2807C19.542 7.15186 15.8122 5 12 5C8.18777 5 4.45796 7.15186 2.17003 11.2807C1.94637 11.6844 1.94361 12.1821 2.16029 12.5876C4.41183 16.8013 8.1628 19 12 19C15.8372 19 19.5882 16.8013 21.8397 12.5876C22.0564 12.1821 22.0536 11.6844 21.83 11.2807ZM12 17C9.06097 17 6.04052 15.3724 4.09173 11.9487C6.06862 8.59614 9.07319 7 12 7C14.9268 7 17.9314 8.59614 19.9083 11.9487C17.9595 15.3724 14.939 17 12 17Z" /></svg></span>
+          <span className="ml-1">{article.views}</span>
+        </div>
+        <div className="flex flex-wrap items-center">
+          <span><svg className="w-6 h-6 fill-white stroke-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.49999 20.25C4.37892 20.2521 4.25915 20.2248 4.1509 20.1705C4.04266 20.1163 3.94916 20.0366 3.87841 19.9383C3.80766 19.8401 3.76175 19.7261 3.74461 19.6063C3.72747 19.4864 3.73961 19.3641 3.77999 19.25L5.37999 14C5.03175 13.0973 4.85539 12.1375 4.85999 11.17C4.8584 10.1057 5.06918 9.0518 5.47999 8.06999C5.88297 7.13047 6.45975 6.27549 7.17999 5.54999C7.90382 4.82306 8.76344 4.24545 9.70999 3.84999C10.6889 3.4344 11.7415 3.22021 12.805 3.22021C13.8685 3.22021 14.9211 3.4344 15.9 3.84999C17.3341 4.46429 18.5573 5.48452 19.4191 6.7851C20.2808 8.08568 20.7434 9.60985 20.75 11.17C20.7437 13.2771 19.9065 15.2966 18.42 16.79C17.6945 17.5102 16.8395 18.087 15.9 18.49C14.0091 19.2819 11.8865 19.3177 9.96999 18.59L4.71999 20.19C4.64977 20.22 4.57574 20.2402 4.49999 20.25ZM12.8 4.74999C11.5334 4.75547 10.2962 5.13143 9.24068 5.83153C8.18519 6.53164 7.35763 7.52528 6.85999 8.68999C6.19883 10.2911 6.19883 12.0889 6.85999 13.69C6.91957 13.8548 6.91957 14.0352 6.85999 14.2L5.62999 18.37L9.77999 17.11C9.94477 17.0504 10.1252 17.0504 10.29 17.11C11.0824 17.439 11.932 17.6083 12.79 17.6083C13.648 17.6083 14.4976 17.439 15.29 17.11C16.0708 16.7813 16.779 16.3018 17.3742 15.6989C17.9693 15.096 18.4397 14.3816 18.7583 13.5967C19.077 12.8118 19.2376 11.9717 19.231 11.1245C19.2244 10.2774 19.0508 9.4399 18.72 8.65999C18.2234 7.50094 17.398 6.51285 16.3459 5.81792C15.2937 5.123 14.0609 4.75171 12.8 4.74999Z"/></svg></span>
+          <span className="ml-1">{article.comments}</span>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Link className="p-6 bg-cyan-800 hover:bg-cyan-700 text-white text-center w-1/3 rounded" href={viewUrl}>View</Link>
+      </div>
     </div>
   )
 }
 
-const Content: React.FC = () => {
+const Content: React.FC<{ limit?: number, cdn: string }> = ({ limit=10, cdn }) => {
   const [requireItems, setRequireItems] = useState(true);
 
   const { data, fetchNextPage } = api.blog.getAll.useInfiniteQuery({
-    limit: 2
+    limit: limit
   },
   {
     getNextPageParam: (lastPage) => lastPage.nextCur,
@@ -36,18 +60,20 @@ const Content: React.FC = () => {
     data.pages.forEach((pg) => {
       items.push(...pg.items);
 
-      if (pg.items.length < 1)
+      if (pg.items.length < limit && requireItems)
         setRequireItems(false);
     });
   }
 
   return (
     <div className="content">
+      <h1 className="content-title">Blog</h1>
       <InfiniteScroll
         pageStart={0}
         loadMore={loadMore}
-        loader={<Loader />}
+        loader={<Loader key={"loader"} />}
         hasMore={requireItems}
+        className={"grid-view grid-view-center grid-view-lg"}
       >
         <>
           {data && (
@@ -56,6 +82,7 @@ const Content: React.FC = () => {
                 return (
                   <ArticleRow
                     key={"article-" + article.id}
+                    cdn={cdn}
                     article={article}
                   />
                 )
@@ -68,11 +95,23 @@ const Content: React.FC = () => {
   )
 }
 
-const Page: NextPage = () => {
+const Page: NextPage<{ cdn: string }> = ({ cdn }) => {
   return (
     <Deaconn 
-      content={<Content />}
+      content={<Content 
+        cdn={cdn}
+      />}
     />
   );
 };
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  let cdn = "";
+
+  if (process.env.CDN_URL)
+    cdn = process.env.CDN_URL;
+
+  return { props: { cdn: cdn } };
+}
+
 export default Page;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,42 @@
-import { type NextPage } from "next";
+import { Article, Service, User } from "@prisma/client";
+import { GetServerSidePropsContext, type NextPage } from "next";
+import { prisma } from "~/server/db";
 import { Deaconn } from '../components/main';
+import { ArticleRow } from "./blog";
 
-const Content: React.FC = () => {
+const Content: React.FC<{ cdn: string, articles: Article[], team: User[], services: Service[]}> = ({ cdn, articles, team, services }) => {
   return (
     <div className="content">
       <div className="flex flex-wrap">
         <div className="content-col-large">
           <div className="w-full">
-            <h3 className="text-white text-3xl font-bold italic">Popular Services</h3>
+            <h1 className="content-title">Popular Services</h1>
             <div>
               <p>Our services!</p>
             </div>
           </div>
           <div className="w-full">
-            <h3 className="text-white text-3xl font-bold italic">Have A Request?</h3>
+            <h1 className="content-title">Have A Request?</h1>
           </div>
           <div className="w-full">
-            <h3 className="text-white text-3xl font-bold italic">Latest Articles</h3>
+            <h1 className="content-title">Latest Articles</h1>
+            <div className="grid-view grid-view-sm">
+              {articles.map((article) => {
+                return (
+                  <ArticleRow
+                    small={true}
+                    key={"article-" + article.id}
+                    cdn={cdn}
+                    article={article}
+                  />
+                )
+              })}
+            </div>
           </div>
         </div>
         <div className="content-col-small">
           <div className="w-full">
-            <h3 className="text-white text-3xl font-bold italic">Our Team</h3>
+            <h1 className="content-title">Our Team</h1>
           </div>
         </div>
       </div>
@@ -29,12 +44,53 @@ const Content: React.FC = () => {
   );
 }
 
-const Page: NextPage = () => {
+const Page: NextPage<{ cdn: string, articles: Article[], team: User[], services: Service[]}> = ({ cdn, articles, team, services }) => {
   return (
     <Deaconn 
-      content={<Content />}
+      content={<Content 
+        cdn={cdn}
+        articles={articles}
+        team={team}
+        services={services}
+      />}
     />
   );
 };
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  let cdn = "";
+
+  if (process.env.CDN_URL)
+    cdn = process.env.CDN_URL;
+
+  // Retrieve articles, team members, and services.
+  const articles = await prisma.article.findMany({
+    take: 10,
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+
+  const team = await prisma.user.findMany({
+    take: 10,
+    where: {
+      isTeam: true
+    }
+  });
+
+  const services = await prisma.service.findMany({
+    take: 10,
+    orderBy: {
+      downloads: "desc"
+    }
+  });
+
+  return { props: { 
+    cdn: cdn,
+    articles: JSON.parse(JSON.stringify(articles)),
+    team: JSON.parse(JSON.stringify(team)),
+    services: JSON.parse(JSON.stringify(services))
+  } };
+}
 
 export default Page;
