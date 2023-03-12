31c08647f11a3ed8e684a20e3a0ecc770ca2fabb
Christian Deacon <gamemann@gflclan.com>
2023-03-11 22:04:16 -0600

    Improve footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,18 @@
+import { Partner, Service } from "@prisma/client";
 import Link from "next/link";
+import { api } from "~/utils/api";
 
 export const Footer: React.FC = () => {
+    const partnerQuery = api.partner.getAll.useQuery({
+        limit: 10
+    });
+    const partners = partnerQuery.data?.items;
+
+    const serviceQuery = api.service.getAll.useQuery({
+        limit: 10
+    });
+    const services = serviceQuery.data?.items;
+
     return (
         <footer className="p-18 bg-slate-800">
             <div className="container mx-auto flex flex-wrap text-center">
@@ -12,6 +24,7 @@ export const Footer: React.FC = () => {
                             <li className="footer-link"><Link href="/service">Services</Link></li>
                             <li className="footer-link"><Link href="/blog">Blog</Link></li>
                             <li className="footer-link"><Link href="/about">About Us</Link></li>
+                            <li className="footer-link"><Link href="/request">My Requests</Link></li>
                             <li className="footer-link"><Link href="/request/new">New Request</Link></li>
                         </ul>
                     </div>
@@ -19,13 +32,37 @@ export const Footer: React.FC = () => {
                 <div className="footer-section">
                     <div>
                         <h3 className="footer-title">Services</h3>
+                        <ul className="list-none">
+                            {services && (
+                                <>
+                                    {services.map((service: Service) => {
+                                        const viewLink = "/service/view/" + service.url;
+
+                                        return (
+                                            <li key={"footer-service-" + service} className="footer-link"><Link href={viewLink}>{service.name}</Link></li> 
+                                        );   
+                                    })}
+      
+                                </>
+      
+                            )}
+                        </ul>
                     </div>
                 </div>
                 <div className="footer-section">
                     <div>
                         <h3 className="footer-title">Partners</h3>
                         <ul className="list-none">
-                            <li className="footer-link"><a href="https://moddingcommunity.com" target="_blank">The Modding Community</a></li>
+                            {partners && (
+                                <>
+                                    {partners.map((partner: Partner) => {
+                                        return (
+                                            <li key={"footer-partner-" + partner.id} className="footer-link"><a href={partner.url} target="_blank">{partner.name}</a></li>
+                                        );
+                                    })}
+                                </>
+                            )}
+            
                         </ul>
                     </div>
                 </div>
