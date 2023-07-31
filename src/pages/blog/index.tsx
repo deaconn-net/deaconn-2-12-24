24fc1a89d1a08b48d3c7a351b4a1882bacd4e4f2
Christian Deacon <christianmdeacon@gmail.com>
2023-07-31 05:56:26 +0000

    Add an add icon for blog and service index.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -11,6 +11,7 @@ import { type Article } from "@prisma/client";
 
 import { api } from "@utils/api";
 import Loader from "@utils/loader";
+import AddIcon from "@utils/icons/add";
 
 import InfiniteScroll from "react-infinite-scroller";
 
@@ -64,7 +65,7 @@ const Page: NextPage = () => {
                 title="Blog - Deaconn"
             />
             <div className="content">
-                <h1 className="content-title">Blog</h1>
+                <h1>Blog</h1>
                 <div className="p-6 flex flex-wrap justify-between">
                     <div className="flex gap-2">
                         <Link href="#" className={"button" + ((mostPopular) ? " !bg-cyan-600" : "")} onClick={(e) => {
@@ -86,7 +87,9 @@ const Page: NextPage = () => {
                         }}>Oldest</Link>
                     </div>
                     <Link href="/blog/new" className="button button-secondary flex">
-                        <span><svg className="w-6 h-6 fill-none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path opacity="0.1" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" className="fill-white" /><path d="M9 12H15" className="stroke-white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /><path d="M12 9L12 15" className="stroke-white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /><path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" className="stroke-white" strokeWidth="2" /></svg></span>
+                        <AddIcon 
+                            classes={["w-6", "h-6", "fill-none"]}
+                        />
                         <span className="ml-2">New Article</span>
                     </Link>
                 </div>
--- src/pages/service/index.tsx
+++ src/pages/service/index.tsx
@@ -9,6 +9,7 @@ import Wrapper from "@components/wrapper";
 
 import { api } from "@utils/api";
 import Loader from "@utils/loader";
+import AddIcon from "@utils/icons/add";
 
 import InfiniteScroll from "react-infinite-scroller";
 
@@ -53,7 +54,7 @@ const Page: NextPage = () => {
     return (
         <Wrapper>
             <div className="content">
-                <h1 className="content-title">Services</h1>
+                <h1>Services</h1>
                 <div className="p-6 flex justify-between">
                     <Link href="#" className={"button" + ((mostPopular) ? " !bg-cyan-600" : "")} onClick={(e) => {
                         e.preventDefault();
@@ -64,7 +65,9 @@ const Page: NextPage = () => {
                             setMostPopular(true);
                     }}>Most Popular</Link>
                     <Link href="/service/new" className="button button-secondary flex">
-                        <span><svg className="w-6 h-6 fill-none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path opacity="0.1" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" className="fill-white" /><path d="M9 12H15" className="stroke-white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /><path d="M12 9L12 15" className="stroke-white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /><path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" className="stroke-white" strokeWidth="2" /></svg></span>
+                        <AddIcon 
+                            classes={["w-6", "h-6", "fill-none"]}
+                        />
                         <span className="ml-2">New Service</span>
                     </Link>
                 </div>
