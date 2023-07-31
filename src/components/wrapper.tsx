410d028a991bc8b7ab8c03b7adf53547151144d9
Christian Deacon <christianmdeacon@gmail.com>
2023-07-31 06:08:30 +0000

    Remove unused code from wrapper.

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -8,15 +8,6 @@ const Wrapper: React.FC<{
 }> = ({
     children
 }) => {
-    // Retrieve URLs.
-    let base_url;
-    let full_url;
-
-    if (typeof window !== "undefined") {
-        base_url = window.location.protocol + "//" + window.location.host;
-        full_url = base_url + window.location.pathname;
-    }
-
     return (
         <main>
             <Header />
