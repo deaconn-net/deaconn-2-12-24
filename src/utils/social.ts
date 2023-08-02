30105499d6c05b3f325a373ebc36a394ad06c5b3
Christian Deacon <christianmdeacon@gmail.com>
2023-08-02 18:20:33 +0000

    Add social util for parsing and retrieving socials tags/URLs.

---
+++ src/utils/social.ts
@@ -0,0 +1,87 @@
+const TWITTER_URL = "twitter.com";
+const GITHUB_URL = "github.com";
+const LINKEDIN_URL = "linkedin.com";
+const FACEBOOK_URL = "facebook.com";
+
+export function FormatSocialUrl(
+    tag: string,
+    platform: string
+): string {
+    let ret = tag;
+
+    switch (platform.toLowerCase()) {
+        case "twitter":
+            ret = `https://${TWITTER_URL}/${tag}`
+
+            break;
+
+        case "github":
+            ret = `https://${GITHUB_URL}/${tag}`
+
+            break;
+
+        case "linkedin":
+            ret = `https://${LINKEDIN_URL}/in/${tag}`
+
+            break;
+
+        case "facebook":
+            ret = `https://${FACEBOOK_URL}/${tag}`
+
+            break;
+
+        // Website, a special case.
+        case "website":
+            // See if we need to prepend protocol.
+            if (!ret.includes("https://") && !ret.includes("http://"))
+                ret = `https://${ret}`;
+
+            break;
+    }
+
+    return ret;
+}
+
+export function RetrieveSocialTag(
+    input: string,
+    platform: string
+): string | undefined {
+    let tag: string | undefined = undefined;
+
+    if (input.length < 1)
+        return tag;
+
+    tag = input;
+
+    // Strip out protocols.
+    tag = tag.replaceAll("https://", "");
+    tag = tag.replaceAll("http://", "");
+
+    // Replace double slashes with one just in-case.
+    tag = tag.replaceAll("//", "/");
+
+    // Now strip out URLs based off of platform parameter.
+    switch (platform.toLowerCase()) {
+        case "twitter":
+            tag = tag.replaceAll(`${TWITTER_URL}/`, ``);
+
+            break;
+
+        case "github":
+            tag = tag.replaceAll(`${GITHUB_URL}/`, ``);
+
+            break;
+
+        case "linkedin":
+            tag = tag.replaceAll(`${LINKEDIN_URL}/in/`, ``);
+
+            break;
+
+        case "facebook":
+            tag = tag.replaceAll(`${FACEBOOK_URL}/`, ``)
+
+            break;
+    }
+
+    return tag;
+}
