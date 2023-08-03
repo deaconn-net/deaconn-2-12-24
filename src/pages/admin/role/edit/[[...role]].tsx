7966626c92e2c9db8047375fc6525d3680d2b0ac
Christian Deacon <christianmdeacon@gmail.com>
2023-08-03 06:31:37 +0000

    Add new admin area that still needs improvements.

---
+++ src/pages/admin/role/edit/[[...role]].tsx
@@ -0,0 +1,79 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+import { getSession } from "next-auth/react";
+
+import { type Role } from "@prisma/client"
+
+import { prisma } from "@server/db";
+
+import NoPermissions from "@components/errors/no_permissions";
+import Wrapper from "@components/wrapper";
+import NotFound from "@components/errors/not_found";
+import RoleForm from "@components/forms/role/new";
+
+import { has_role } from "@utils/user/auth";
+
+const Edit: NextPage<{
+    authed: boolean,
+    role: Role | null
+}> = ({
+    authed,
+    role
+}) => {
+    if (!authed) {
+        return (
+            <Wrapper>
+                <NoPermissions />
+            </Wrapper>
+        );
+    }
+
+    if (!role) {
+        return (
+            <Wrapper>
+                <NotFound item="Role" />
+            </Wrapper>
+        );
+    }
+
+    return (
+        <Wrapper>
+            <RoleForm
+                role={role}
+            />
+        </Wrapper>
+    );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    const session = await getSession(ctx);
+
+    // Check if we have permissions.
+    let authed = false;
+
+    if (session && has_role(session, "admin"))
+        authed = true;
+
+    // Retrieve role we're editing.
+    const { params } = ctx;
+
+    const roleId = params?.role?.[0];
+
+    let role: Role | null = null;
+
+    if (roleId) {
+        role = await prisma.role.findFirst({
+            where: {
+                id: roleId
+            }
+        })
+    }
+
+    return {
+        props: {
+            authed: authed,
+            role: role
+        }
+    };
+}
+
+export default Edit;
