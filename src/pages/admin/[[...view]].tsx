36b8549a4b05ccafffcf5e59249d18c891deaa4b
Christian Deacon <christianmdeacon@gmail.com>
2023-08-03 16:49:15 +0000

    Utilize admin settings panel component on all admin pages.

--- src/pages/admin/[[...view]].tsx
+++ src/pages/admin/[[...view]].tsx
@@ -15,6 +15,7 @@ import { has_role } from "@utils/user/auth";
 
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import RoleForm from "@components/forms/role/new";
+import AdminSettingsPanel from "@components/admin/settingspanel";
 
 type statsType = {
     articles?: number
@@ -40,101 +41,83 @@ const Page: NextPage<{
             <Wrapper>
                 {authed ? (
                     <div className="content-item">
-                        <div className="flex flex-wrap gap-2">
-                            <div>
-                                <ul className="tab-container w-64">
-                                    <Link
-                                        href="/admin"
-                                        className={`tab-link ${view == "general" ? "tab-active" : ""}`}
-                                    >General</Link>
-                                    <Link
-                                        href="/admin/roles"
-                                        className={`tab-link ${view == "roles" ? "tab-active" : ""}`}
-                                    >Roles</Link>
-                                    <Link
-                                        href="/admin/users"
-                                        className={`tab-link ${view == "users" ? "tab-active" : ""}`}
-                                    >Users</Link>
-                                </ul>
-                            </div>
-                            <div className="grow p-6 bg-gray-800 rounded-sm flex flex-col gap-4">
-                                {view == "general" && (
-                                    <div className="flex flex-col gap-4">
-                                        <div className="flex flex-wrap justify-center gap-4">
-                                            {stats && (
-                                                <div className="content-item">
-                                                    <h2>Stats</h2>
-                                                    <ul>
-                                                        <li><span className="font-bold">{stats.articles}</span> Total Articles</li>
-                                                        <li><span className="font-bold">{stats.services}</span> Total Services</li>
-                                                        <li><span className="font-bold">{stats.users}</span> Total Users</li>
-                                                    </ul>
-                                                </div>
-                                            )}
-                                        </div>
+                        <AdminSettingsPanel view={view}>
+                            {view == "general" && (
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-wrap justify-center gap-4">
+                                        {stats && (
+                                            <div className="content-item">
+                                                <h2>Stats</h2>
+                                                <ul>
+                                                    <li><span className="font-bold">{stats.articles}</span> Total Articles</li>
+                                                    <li><span className="font-bold">{stats.services}</span> Total Services</li>
+                                                    <li><span className="font-bold">{stats.users}</span> Total Users</li>
+                                                </ul>
+                                            </div>
+                                        )}
                                     </div>
-                                )}
-                                {view == "roles" && (
-                                    <div className="flex flex-col gap-4">
-                                        <div>
-                                            <RoleForm />
-                                        </div>
-                                        <div className="flex gap-4">
-                                            {roles?.map((role) => {
-                                                // Compile links.
-                                                const editUrl = `/admin/role/edit/${role.id}`;
+                                </div>
+                            )}
+                            {view == "roles" && (
+                                <div className="flex flex-col gap-4">
+                                    <div>
+                                        <RoleForm />
+                                    </div>
+                                    <div className="flex gap-4">
+                                        {roles?.map((role) => {
+                                            // Compile links.
+                                            const editUrl = `/admin/role/edit/${role.id}`;
 
-                                                return (
-                                                    <div
-                                                        key={`admin-roles-${role.id}`}
-                                                        className="p-6 bg-gray-900 flex flex-col gap-2 rounded-md"
-                                                    >
-                                                        <div className="flex gap-2 items-center">
-                                                            <h2 className="text-center">{role.title}</h2>
-                                                            <span className="text-sm">{` (${role.id})`}</span>
-                                                        </div>
-                                                        <div>
-                                                            {role.desc ? (
-                                                                <ReactMarkdown className="markdown">
-                                                                    {role.desc}
-                                                                </ReactMarkdown>
-                                                            ) : (
-                                                                <p className="italic">No description available.</p>
-                                                            )}
-                                                        </div>
-                                                        <div className="flex flex-wrap gap-2">
-                                                            <Link
-                                                                href={editUrl}
-                                                                className="button button-primary"
-                                                            >Edit</Link>
-                                                            <button
-                                                                className="button button-danger"
-                                                                onClick={(e) => {
-                                                                    e.preventDefault();
-
-                                                                    const yes = confirm("Are you sure you want to delete this role?");
-
-                                                                    if (yes) {
-                                                                        roleDeleteMut.mutate({
-                                                                            role: role.id
-                                                                        });
-                                                                    }
-                                                                }}
-                                                            >Delete</button>
-                                                        </div>
+                                            return (
+                                                <div
+                                                    key={`admin-roles-${role.id}`}
+                                                    className="p-6 bg-gray-900 flex flex-col gap-2 rounded-md"
+                                                >
+                                                    <div className="flex gap-2 items-center">
+                                                        <h2 className="text-center">{role.title}</h2>
+                                                        <span className="text-sm">{` (${role.id})`}</span>
                                                     </div>
-                                                );
-                                            })}
-                                        </div>
+                                                    <div>
+                                                        {role.desc ? (
+                                                            <ReactMarkdown className="markdown">
+                                                                {role.desc}
+                                                            </ReactMarkdown>
+                                                        ) : (
+                                                            <p className="italic">No description available.</p>
+                                                        )}
+                                                    </div>
+                                                    <div className="flex flex-wrap gap-2">
+                                                        <Link
+                                                            href={editUrl}
+                                                            className="button button-primary"
+                                                        >Edit</Link>
+                                                        <button
+                                                            className="button button-danger"
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+
+                                                                const yes = confirm("Are you sure you want to delete this role?");
+
+                                                                if (yes) {
+                                                                    roleDeleteMut.mutate({
+                                                                        role: role.id
+                                                                    });
+                                                                }
+                                                            }}
+                                                        >Delete</button>
+                                                    </div>
+                                                </div>
+                                            );
+                                        })}
                                     </div>
-                                )}
-                                {view == "users" && (
-                                    <div className="flex flex-col gap-4">
-                                        <UserBrowser />
-                                    </div>
-                                )}
-                            </div>
-                        </div>
+                                </div>
+                            )}
+                            {view == "users" && (
+                                <div className="flex flex-col gap-4">
+                                    <UserBrowser />
+                                </div>
+                            )}
+                        </AdminSettingsPanel>
                     </div>
                 ) : (
                     <NoPermissions />
