2e16737a1af75f9806508b1c448b0a7cadbde5ed
Christian Deacon <christianmdeacon@gmail.com>
2023-08-17 05:07:48 +0000

    Add custom request type which includes service, replies, and user.

--- src/types/request.tsx
+++ src/types/request.tsx
@@ -4,4 +4,16 @@ export type RequestWithService = Prisma.RequestGetPayload<{
     include: {
         service: true
     }
-}>;
+}>
+
+export type RequestWithAll = Prisma.RequestGetPayload<{
+    include: {
+        service: true,
+        replies: {
+            include: {
+                user: true
+            }
+        },
+        user: true
+    }
+}>
