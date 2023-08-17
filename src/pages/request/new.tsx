cbed192b87ad38bb8a802c1ff6d62dc98fb27448
Christian Deacon <christianmdeacon@gmail.com>
2023-08-17 05:08:43 +0000

    Retrieve services when creating new request.

--- src/pages/request/new.tsx
+++ src/pages/request/new.tsx
@@ -1,23 +1,25 @@
 import { type GetServerSidePropsContext, type NextPage } from "next";
 import { getSession } from "next-auth/react";
 
-import { type Request } from "@prisma/client";
+import { Service, type Request } from "@prisma/client";
 
 import { prisma } from "@server/db";
 
 import Wrapper from "@components/wrapper";
 import Meta from "@components/meta";
 
-import Form from '@components/forms/request/new';
+import RequestForm from '@components/forms/request/new';
 import NoPermissions from "@components/errors/no_permissions";
 import GlobalProps, { type GlobalPropsType } from "@utils/global_props";
 
 const Page: NextPage<{
     authed: boolean,
-    request?: Request
+    request?: Request,
+    services?: Service[]
 } & GlobalPropsType> = ({
     authed,
     request,
+    services,
 
     footerServices,
     footerPartners
@@ -35,8 +37,9 @@ const Page: NextPage<{
                 {authed ? (
                     <div className="content-item">
                         <h1>{request ? "Save" : "Create"} Request</h1>
-                        <Form
+                        <RequestForm
                             request={request}
+                            services={services}
                         />
                     </div>
                 ) : (
@@ -61,6 +64,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     const lookup_id = query?.id;
 
     let request: Request | null = null;
+    let services: Service[] | null = null;
 
     if (lookup_id) {
         request = await prisma.request.findFirst({
@@ -70,13 +74,18 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
         });
     }
 
+    if (authed) {
+        services = await prisma.service.findMany();
+    }
+
     const globalProps = await GlobalProps();
 
     return {
         props: {
             ...globalProps,
             authed: authed,
-            request: request
+            request: request,
+            services: JSON.parse(JSON.stringify(services))
         }
     };
 }
