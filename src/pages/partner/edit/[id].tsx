81d33dc3cbfb4a48aaaad99f2d7ceca86f07a0f7
Christian Deacon <christianmdeacon@gmail.com>
2023-08-20 22:46:30 +0000

    Restructure partner pages.

--- src/pages/partner/edit/[id].tsx
+++ src/pages/partner/edit/[id].tsx
@@ -3,6 +3,8 @@ import { getSession } from "next-auth/react";
 
 import { type Partner } from "@prisma/client";
 
+import { prisma } from "@server/db";
+
 import Wrapper from "@components/wrapper";
 import Meta from "@components/meta";
 
@@ -11,33 +13,43 @@ import NoPermissions from "@components/errors/no_permissions";
 
 import { has_role } from "@utils/user/auth";
 import GlobalProps, { type GlobalPropsType } from "@utils/global_props";
+import NotFound from "@components/errors/not_found";
 
 const Page: NextPage<{
     authed: boolean,
     partner?: Partner
 } & GlobalPropsType> = ({
     authed,
-
+    partner,
+    
     footerServices,
     footerPartners
 }) => {
     return (
         <>
             <Meta
-                title="New Partner - Partners - Deaconn"
+                title={`Editing Partner ${partner?.name ?? "N/A"} - Partners - Deaconn`}
                 robots="noindex"
             />
             <Wrapper
                 footerServices={footerServices}
                 footerPartners={footerPartners}
             >
-                {authed ? (
+                {(authed && partner) ? (
                     <div className="content-item">
-                        <h1>Add Partner</h1>
-                        <PartnerForm />
+                        <h1>Edit Partner</h1>
+                        <PartnerForm
+                            partner={partner}
+                        />
                     </div>
                 ) : (
-                    <NoPermissions />
+                    <div className="content-item">
+                        {partner ? (
+                            <NoPermissions />
+                        ) : (
+                            <NotFound item="Partner" />
+                        )}
+                    </div>
                 )}
             </Wrapper>
         </>
@@ -54,13 +66,31 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     if (session && (has_role(session, "contributor") || has_role(session, "admin")))
         authed = true;
 
+    // Retrieve lookup ID.
+    const { params } = ctx;
+
+    const lookupId = params?.id?.toString();
+
+    // Initialize partner.
+    let partner: Partner | null = null;
+
+    // Retrieve partner if we have a lookup ID and are authenticated.
+    if (lookupId && authed) {
+        partner = await prisma.partner.findFirst({
+            where: {
+                id: Number(lookupId)
+            }
+        });
+    }
+
     // Retrieve global props.
     const globalProps = await GlobalProps();
 
     return { 
         props: {
             ...globalProps,
-            authed: authed
+            authed: authed,
+            partner: partner
         }
     };
 }
