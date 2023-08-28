75bc9e23858aa0a90126057fc76cc887b0c133e2
Christian Deacon <christianmdeacon@gmail.com>
2023-08-28 06:42:29 +0000

    Add new router for GitLog.

---
+++ src/server/api/routers/gitlog.ts
@@ -0,0 +1,57 @@
+import { TRPCError } from "@trpc/server";
+import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
+
+import z from "zod";
+
+export const gitLogRouter = createTRPCRouter({
+    getAll: publicProcedure
+        .input(z.object({
+            limit: z.number().default(10),
+            cursor: z.number().nullish()
+        }))
+        .query( async ({ ctx, input }) => {
+            const items = await ctx.prisma.gitLog.findMany({
+                orderBy: {
+                    createdAt: "desc"
+                },
+                include: {
+                    user: true
+                },
+                
+                take: input.limit + 1,
+                cursor: (input.cursor) ? { id: input.cursor } : undefined
+            });
+
+            let nextCur: typeof input.cursor | undefined = undefined;
+
+            if (items.length > input.limit) {
+                const nextItem = items.pop();
+                nextCur = nextItem?.id;
+            }
+
+            return {
+                items,
+                nextCur
+            };
+        }),
+    del: adminProcedure
+        .input(z.object({
+            id: z.number()
+        }))
+        .mutation (async ({ ctx, input }) => {
+            try {
+                await ctx.prisma.gitLog.delete({
+                    where: {
+                        id: input.id
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: `Error deleting git log. Error => ${typeof err == "string" ? err : "Check console"}.`
+                });
+            }
+        })
+})
