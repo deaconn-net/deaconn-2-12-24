784f302bf5205b95427197049a8ea53d149e0b2e
Christian Deacon <christianmdeacon@gmail.com>
2023-08-30 06:48:23 +0000

    Add GitHub stats model and add onto About Us page with stats.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,8 +4,25 @@ import Meta from "@components/Meta";
 import Wrapper from "@components/Wrapper";
 
 import GlobalProps, { type GlobalPropsType } from "@utils/GlobalProps";
+import { prisma } from "@server/db";
 
-const Page: NextPage<GlobalPropsType> = ({
+type stats = {
+    users?: number,
+    articles?: number,
+    services?: number,
+    userExperiences?: number,
+    userSkills?: number,
+    userProjects?: number,
+
+    totalCommits?: number,
+    totalRepos?: number
+}
+
+const Page: NextPage<{
+    stats?: stats
+} & GlobalPropsType> = ({
+    stats,
+
     footerServices,
     footerPartners
 }) => {
@@ -18,12 +35,43 @@ const Page: NextPage<GlobalPropsType> = ({
                 footerServices={footerServices}
                 footerPartners={footerPartners}
             >
-                <div className="content-item2">
-                    <div>
-                        <h1>About Us</h1>
+                <div className="flex flex-wrap sm:flex-nowrap gap-4">
+                    <div className="w-2/3">
+                        <div className="content-item2">
+                            <div>
+                                <h1>About Us</h1>
+                            </div>
+                            <div>
+                                <p>This is the start of our about us page!</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <p>This is the start of our about us page!</p>
+                    <div className="w-1/3">
+                        <div className="content-item2">
+                            <div>
+                                <h1>Stats</h1>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <div>
+                                    <h2>Website</h2>
+                                    <ul className="px-6">
+                                        <li>{stats?.users?.toString() ?? "N/A"} <span className="font-bold">Total Users</span></li>
+                                        <li>{stats?.articles?.toString() ?? "N/A"} <span className="font-bold">Total Articles</span></li>
+                                        <li>{stats?.services?.toString() ?? "N/A"} <span className="font-bold">Total Services</span></li>
+                                        <li>{stats?.userExperiences?.toString() ?? "N/A"} <span className="font-bold">Total User Experiences</span></li>
+                                        <li>{stats?.userSkills?.toString() ?? "N/A"} <span className="font-bold">Total User Skills</span></li>
+                                        <li>{stats?.userProjects?.toString() ?? "N/A"} <span className="font-bold">Total User Projects</span></li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h2>GitHub</h2>
+                                    <ul className="px-6">
+                                        <li>{stats?.totalCommits?.toString() ?? "N/A"} <span className="font-bold">Total Commits</span></li>
+                                        <li>{stats?.totalRepos?.toString() ?? "N/A"} <span className="font-bold">Total Repositories</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Wrapper>
@@ -32,11 +80,39 @@ const Page: NextPage<GlobalPropsType> = ({
 }
 
 export async function getServerSideProps() {
+    const stats: stats = {};
+    stats.users = await prisma.user.count();
+    stats.articles = await prisma.article.count();
+    stats.services = await prisma.service.count();
+    stats.userExperiences = await prisma.userExperience.count();
+    stats.userSkills = await prisma.userSkill.count();
+    stats.userProjects = await prisma.userProject.count();
+
+    // GitHub
+    const commits = await prisma.githubStats.findFirst({
+        where: {
+            key: "commits"
+        }
+    });
+
+    if (commits)
+        stats.totalCommits = Number(commits.val);
+
+    const repos = await prisma.githubStats.findFirst({
+        where: {
+            key: "repositories"
+        }
+    });
+
+    if (repos)
+        stats.totalRepos = Number(repos.val);
+
     const globalProps = await GlobalProps();
 
     return {
         props: {
-            ...globalProps
+            ...globalProps,
+            stats: stats
         }
     }
 }
---
+++ src/pages/api/stats/github.ts
@@ -0,0 +1,112 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "~/server/db";
+
+interface ExtendedNextApiRequest extends NextApiRequest {
+    body: {
+        repositories?: number,
+        commits?: number
+    };
+  }
+
+const statsGithub = async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
+    const method = req.method;
+
+    if (method != "POST") {
+        return res.status(405).json({
+            code: 405,
+            message: "Method not allowed."
+        });
+    }
+
+    if (!req.body) {
+        return res.status(401).json({
+            code: 401,
+            message: "Reqeust body empty!"
+        });
+    }
+
+    const apiKey = process.env.ROOT_API || undefined;
+
+    if ((!apiKey || apiKey.length < 1)) {
+        return res.status(401).json({
+            code: 404,
+            message: "Unauthorized. No GitHub API secret or token set."
+        });
+    }
+
+    const authHeaderVal = req.headers.authorization || undefined;
+
+    if (!authHeaderVal) {
+        return res.status(401).json({
+            code: 401,
+            message: "Authentication header not set."
+        });
+    }
+
+    const apiKeyFull = "Bearer " + apiKey;
+
+    if (authHeaderVal !== apiKeyFull) {
+        return res.status(401).json({
+            code: 404,
+            message: "Unauthorized."
+        });
+    }
+
+    const repos = req.body.repositories; 
+    const commits = req.body.commits;
+
+    if (repos) {
+        try {
+            await prisma.githubStats.upsert({
+                create: {
+                    key: "repositories",
+                    val: repos.toString()
+                },
+                update: {
+                    val: repos.toString()
+                },
+                where: {
+                    key: "repositories"
+                }
+            });
+        } catch (err) {
+            console.error(err);
+
+            return res.status(400).json({
+                code: 400,
+                message: `Error attempting to update repositories. Error => ${typeof err == "string" ? err : "Check console"}.`
+            });
+        }
+    }
+
+    if (commits) {
+        try {
+            await prisma.githubStats.upsert({
+                create: {
+                    key: "commits",
+                    val: commits.toString()
+                },
+                update: {
+                    val: commits.toString()
+                },
+                where: {
+                    key: "commits"
+                }
+            })
+        } catch (err) {
+            console.error(err);
+
+            return res.status(400).json({
+                code: 400,
+                message: `Error attempting to update commits. Error => ${typeof err == "string" ? err : "Check console"}.`
+            });
+        }
+    }
+
+    return res.status(200).json({
+        code: 200,
+        message: `${repos ? "Updated repositories!" : ""} ${commits ? "Updated commits!" : ""}`
+    });
+};
+
+export default statsGithub;
