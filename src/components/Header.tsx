09f9219772c5611109e2dd92988c768ebf9b2ce0
Christian Deacon <christianmdeacon@gmail.com>
2023-09-03 04:37:18 +0000

    Highlight Account header item when on user account pages.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -131,7 +131,7 @@ export default function Header () {
                         <>
                             <Link
                                 href="/user/profile"
-                                className="nav-link" 
+                                className={`nav-link ${location == "/user/profile" ? "nav-active" : ""}`} 
                             >
                                 <li className="nav-item">
                                     <IconAndText
