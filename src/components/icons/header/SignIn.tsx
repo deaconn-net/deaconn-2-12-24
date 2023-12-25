63f7a5f2676e8d41ec163766afa2943195a7d82c
Christian Deacon <christianmdeacon@gmail.com>
2023-12-25 22:38:53 +0000

    Add and replace icons.

--- src/components/icons/header/SignIn.tsx
+++ src/components/icons/header/SignIn.tsx
@@ -4,8 +4,8 @@ export default function SignInIcon ({
     className?: string
 }) {
     return (
-        <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4H11M3 12H14M14 12L11 15M14 12L11 9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <svg className={className} viewBox="0 0 512 512">
+            <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
         </svg>
     );
 }
