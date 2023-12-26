f0ab3bc5088ef129f974c4750e440c9b8a7fc9ce
Christian Deacon <christianmdeacon@gmail.com>
2023-12-26 05:27:28 +0000

    Add view port context to wrapper component.

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 
 import { ErrorCtx, SuccessCtx } from "@pages/_app";
@@ -11,6 +11,14 @@ import ErrorBox from "@components/error/Box";
 import SuccessBox from "@components/success/Box";
 import Breadcrumbs, { type Breadcrumb } from "./Breadcrumbs";
 import GoogleAnalytics from "./GoogleAnalytics";
+
+export const ViewPortCtx = createContext({
+    mobile: false,
+    innerHeight: 0,
+    innerWidth: 0,
+    scrollY: 0,
+    scrollX: 0
+})
 
 export default function Wrapper ({
     footerServices,
@@ -54,53 +62,90 @@ export default function Wrapper ({
         }
     }, [errorCtx, successCtx]);
 
-    const [windowY, setWindowY] = useState(0);
+    const [viewPort, setViewPort] = useState({
+        mobile: false,
+        innerWidth: 0,
+        innerHeight: 0,
+        scrollY: 0,
+        scrollX: 0
+    });
 
     useEffect(() => {
+        const checkViewPort = () => {
+            if (typeof window !== undefined) {
+                const innerHeight = window.innerHeight;
+                const innerWidth = window.innerWidth;
+
+                const scrollY = window.scrollY;
+                const scrollX = window.scrollX;
+
+                setViewPort({
+                    mobile: innerWidth <= 630,
+                    innerHeight: innerHeight,
+                    innerWidth: innerWidth,
+                    scrollY: scrollY,
+                    scrollX: scrollX
+                })
+            }
+        }
+
         if (typeof window !== undefined) {
-            setWindowY(window.scrollY);
+            window.addEventListener("scroll", checkViewPort);
+            window.addEventListener("resize", checkViewPort);
+        }
+
+        // Check view port now.
+        checkViewPort();
+
+        return () => {
+            if (typeof window !== undefined) {
+                window.removeEventListener("scroll", checkViewPort);
+                window.removeEventListener("resize", checkViewPort);
+            }
         }
     }, [])
 
     return (
-        <main>
-            <GoogleAnalytics />
-            <Header />
-            <div className={`content ${(windowY == 0 && firstRender) ? "sm:animate-content-slide-up" : ""}`}>
-                {breadcrumbs && (
-                    <Breadcrumbs breadcrumbs={breadcrumbs} />
-                )}
-                {session?.user?.isRestricted ? (
-                    <div className="content-item2">
-                        <div>
-                            <h1>Restricted!</h1>
+        <main className="text-gray-100 min-h-screen bg-gradient-to-b from-zinc-900 to-neutral-900 flex flex-col break-words">
+            <ViewPortCtx.Provider value={viewPort}>
+                <GoogleAnalytics />
+                <Header />
+                <div className={`content ${(viewPort.scrollY == 0 && firstRender.current) ? "sm:animate-content-slide-up" : ""}`}>
+                    {breadcrumbs && (
+                        <Breadcrumbs breadcrumbs={breadcrumbs} />
+                    )}
+                    {session?.user?.isRestricted ? (
+                        <div className="content-item2">
+                            <div>
+                                <h1>Restricted!</h1>
+                            </div>
+                            <div>
+                                <p>Your account has been restricted. Please contact an administrator.</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Your account has been restricted. Please contact an administrator.</p>
-                        </div>
-                    </div>
-                ) : (
-                    <>
-                        <ErrorBox
-                            title={errorCtx?.title}
-                            msg={errorCtx?.msg}
-                        />
-                        <SuccessBox
-                            title={successCtx?.title}
-                            msg={successCtx?.msg}
-                        />
+                    ) : (
+                        <>
+                            <ErrorBox
+                                title={errorCtx?.title}
+                                msg={errorCtx?.msg}
+                            />
+                            <SuccessBox
+                                title={successCtx?.title}
+                                msg={successCtx?.msg}
+                            />
 
-                        {children}
-                    </>
-                )}
-                {breadcrumbs && (
-                    <Breadcrumbs breadcrumbs={breadcrumbs} />
-                )}
-            </div>
-            <Footer
-                services={footerServices}
-                partners={footerPartners}
-            />
+                            {children}
+                        </>
+                    )}
+                    {breadcrumbs && (
+                        <Breadcrumbs breadcrumbs={breadcrumbs} />
+                    )}
+                </div>
+                <Footer
+                    services={footerServices}
+                    partners={footerPartners}
+                />
+            </ViewPortCtx.Provider>
         </main>
     );
 }
