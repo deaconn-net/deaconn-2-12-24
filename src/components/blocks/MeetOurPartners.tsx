e25538b7f4ebdcd705fdb682d9f2d42e79ed40c3
Christian Deacon <christianmdeacon@gmail.com>
2023-12-27 04:17:13 +0000

    Complete meet our partners block.

--- src/components/blocks/MeetOurPartners.tsx
+++ src/components/blocks/MeetOurPartners.tsx
@@ -1,19 +1,57 @@
+import Markdown from "@components/markdown/Markdown";
 import { type Partner } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function MeetOurPartnersBlock ({
-    partners
+    partners = []
 } : {
     partners?: Partner[]
 }) {
+    const uploadsUrl = process.env.NEXT_PUBLIC_UPLOADS_URL ?? "";
+
     return (
         <>
-            {partners && partners.length > 0 && (
+            {partners.length > 0 && (
                 <div className="content-item2">
                     <div>
                         <h2>Meet Our Partners!</h2>
                     </div>
                     <div>
-                        <p className="italic">Under construction...</p>
+                        {partners.map((partner, index) => {
+                            return (
+                                <div
+                                    key={`partner-${index.toString()}`}
+                                    className="p-2"
+                                >
+                                    <div className="flex flex-wrap gap-2 items-center">
+                                        {partner.icon && (
+                                            <Image
+                                                src={uploadsUrl + partner.icon}
+                                                width={32}
+                                                height={32}
+                                                alt="Partner Icon"
+                                            />
+                                        )}
+                                        <h4 className="not-italic">
+                                            <Link
+                                                href={`https://${partner.url}`}
+                                                target="_blank"
+                                            >{partner.name}</Link>
+                                        </h4>
+                                    </div>
+                                    <div>
+                                        {partner.about ? (
+                                            <Markdown>
+                                                {partner.about}
+                                            </Markdown>
+                                        ) : (
+                                            <p className="italic">No information provided...</p>
+                                        )}
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             )}
