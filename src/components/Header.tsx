1fae51afd2a69953b88de9bf36399dcdde147d79
Christian Deacon <christianmdeacon@gmail.com>
2023-12-27 03:04:05 +0000

    Improve navbar and add animations.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -48,7 +48,52 @@ export default function Header () {
     const router = useRouter();
     const location = router.pathname;
 
+    // Figure out if this is our first render.
+    const firstRender = useRef(true);
+
+    useEffect(() => {
+        if (firstRender.current)
+            firstRender.current = false;
+    }, [firstRender])
+
+    const isFirstRender = firstRender.current;
+
     const [mobileOpen, setMobileOpen] = useState(false);
+    const mobileMenu = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const ele = mobileMenu.current;
+
+        if (!ele)
+            return;
+
+        if (mobileOpen) {
+            ele.classList.remove("hidden");
+            ele.classList.remove("animate-menu-right-to-left");
+
+            ele.classList.add("flex");
+            ele.classList.add("animate-menu-left-to-right");
+        } else if (!isFirstRender) {
+            ele.classList.remove("animate-menu-left-to-right");
+            ele.classList.add("animate-menu-right-to-left");
+        }
+
+        const onAnimateEnd = (e: AnimationEvent) => {
+            if (e.animationName == "menu-right-to-left") {
+                ele.classList.remove("flex");
+                ele.classList.add("hidden");
+            }
+        }
+
+        ele.addEventListener("animationend", onAnimateEnd, {
+            once: true
+        });
+
+        return () => {
+            ele.removeEventListener("animationend", onAnimateEnd);
+        }
+    }, [mobileOpen, isFirstRender])
+
 
     const viewPort = useContext(ViewPortCtx);
 
@@ -68,7 +113,7 @@ export default function Header () {
                     />
                 )}
             </button>
-            <nav className={`${mobileOpen ? "flex" : "hidden"} flex-col gap-2 bg-deaconn-data overflow-y-auto fixed top-0 left-0 p-2 h-full w-2/3 sm:w-1/2 sm:hidden`}>
+            <nav ref={mobileMenu} className={`hidden flex-col gap-2 bg-deaconn-data2 overflow-auto fixed top-0 left-0 p-2 h-full w-2/3 sm:w-1/2 sm:hidden`}>
                 <div className="flex justify-end">
                     <button
                         type="button"
@@ -179,15 +224,26 @@ export default function Header () {
                 </div>
             </nav>
             <nav className={`hidden container mx-auto sm:flex !flex-nowrap justify-between text-center items-center sm:gap-1 md:gap-4 ${viewPort.scrollY > 0 ? "h-12" : "h-16"}`}>
-                <NavItem link="/">
-                    <Image
-                        src="/images/header_banner.png"
-                        className={viewPort.scrollY > 0 ? "hidden" : "max-h-full w-32 p-2"}
-                        width={128}
-                        height={48}
-                        alt="Deaconn Banner"
-                        priority={true}
-                    />
+                <NavItem className="h-full flex items-center" link="/">
+                    {viewPort.scrollY > 0 ? (
+                        <Image
+                            src="/images/header_icon.png"
+                            className="max-h-full w-12"
+                            width={48}
+                            height={48}
+                            alt="Deaconn Icon"
+                        />
+                    ) : (
+                        <Image
+                            src="/images/header_banner.png"
+                            className="max-h-full w-32"
+                            width={128}
+                            height={64}
+                            alt="Deaconn Banner"
+                            priority={true}
+                        />
+                    )}
+                    
                 </NavItem>
                 <NavItem
                     link="/"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,9 @@ const config = {
         animation: {
             "child-slide-down": "child-slide-down 1s ease-out",
             "child-slide-up": "child-slide-up 1s ease-out",
-            "content-slide-up": "content-slide-up 0.5s ease-out"
+            "content-slide-up": "content-slide-up 0.5s ease-out",
+            "menu-left-to-right": "menu-left-to-right 0.5s",
+            "menu-right-to-left": "menu-right-to-left 0.5s",
         },
         colors: {
             "deaconn-header": "#155e75",
