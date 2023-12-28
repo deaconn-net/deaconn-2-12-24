113f0303c74a8c75b74f305e908b0b8e05b46917
Christian Deacon <christianmdeacon@gmail.com>
2023-12-28 02:31:56 +0000

    Grammar correction.

--- src/components/blocks/WhoAreWe.tsx
+++ src/components/blocks/WhoAreWe.tsx
@@ -10,7 +10,7 @@ export default function WhoAreWeBlock () {
             </div>
             <div>
                 <p>
-                    <span className="font-bold">Deaconn</span> is a community and future software developer with the goal to bring together and empower developers, and engineers.
+                    <span className="font-bold">Deaconn</span> is a community and future software developer with the goal to bring together and empower developers and engineers.
                 </p>
                 <p>
                     Deaconn, initially founded in 2021 as a personal portfolio for <Link href={`/user/view/${rootUserUrl}`}>Christian Deacon</Link>, underwent a complete overhaul in 2023. Its new mission is to create <span className="font-bold">open source</span> services, applications, and frameworks for the benefit of the community. Additionally, Deaconn features a rather unique blog with informative articles, providing users with valuable insights. Users can also showcase their projects, experiences, and skills, creating a dynamic platform akin to a personalized portfolio.  
