780661678f52d444f202aca89f4bdece208ab371
Christian Deacon <christianmdeacon@gmail.com>
2024-01-18 07:57:44 +0000

    Add server-side error component.

---
+++ src/components/error/ServerError.tsx
@@ -0,0 +1,12 @@
+export default function ServerError () {
+    return (
+        <div className="content-item2">
+            <div>
+                <h2>Server-Side Error</h2>
+            </div>
+            <div>
+                <p>The server encountered an error while viewing this page. Please try refreshing the page or contact an administrator if the issue persists.</p>
+            </div>
+        </div>
+    )
+}
