7b7ec476b243e74dca581c7a3848444eac4a5c11
Christian Deacon <christianmdeacon@gmail.com>
2024-01-18 07:57:30 +0000

    Improve Not Found component.

--- src/components/error/NotFound.tsx
+++ src/components/error/NotFound.tsx
@@ -1,16 +1,16 @@
 export default function NotFound ({
-    item = "user"
+    item = "page"
 } : {
     item?: string
 }) {
-    const itemName = item.charAt(0).toUpperCase() + item.slice(1);
     return (
         <div className="content-item2">
             <div>
-                <h2>{itemName} Not Found</h2>
+                <h2>{item.charAt(0).toUpperCase() + item.slice(1)} Not Found</h2>
             </div>
             <div>
-                <p>{itemName} not found. Please check the URL.</p>
+                <p>The {item} you{"'"}re trying to view could not found. Please check the URL.</p>
+                <p>If this is a mistake, please contact an administrator.</p>
             </div>
         </div>
     );
